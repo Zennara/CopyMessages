b10dea7ceb452defff9ac1aaa2aab92c85fea3bf
Keagan Landfried <64995253+Zennara@users.noreply.github.com>
2023-05-21 23:54:56 -0600

    fixed random unknown webhook token

--- main.py
+++ main.py
@@ -13,15 +13,13 @@ f.close()  # close file
 
 guild_ids = config["server_ids"]  # comma separated guild ids
 
-UPDATE_EVERY = 10
+UPDATE_EVERY = 5
+copied_channel = None
 
 
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} Online.")
-
-
-copied_channel = None
 
 
 @bot.slash_command(description="Copy the current channels messages.", guild_ids=guild_ids)
@@ -32,12 +30,25 @@ async def copy(ctx):
     await ctx.respond(content="Messages Copied", ephemeral=True)
 
 
+async def get_webhook(ctx):
+    webhooks = await ctx.channel.webhooks()
+    history_webhook = None
+    for webhook in webhooks:
+        if webhook.name == "CopyMessages":
+            history_webhook = webhook
+            break
+    else:
+        # create the webhook
+        history_webhook = await ctx.channel.create_webhook(name="CopyMessages", avatar=None, reason="For CopyMessages")
+    return history_webhook
+
+
 @bot.slash_command(description="Paste the saved message data.", guild_ids=guild_ids)
 async def paste(ctx, start_at: Option(int, "The message number to start at. Used for debugging",
                                       required=False, default=1)):
     await ctx.defer()
     all_messages = await copied_channel.history(limit=None, oldest_first=True).flatten()
-    all_messages_count = len(all_messages) - start_at+1
+    all_messages_count = len(all_messages) - start_at + 1
 
     # check start at parameter
     if all_messages_count > len(all_messages):
@@ -53,24 +64,19 @@ async def paste(ctx, start_at: Option(int, "The message number to start at. Used
 
     embed = discord.Embed(color=0xFFFF00,
                           title=f"⏳ Pasting {all_messages_count} Messages",
-                          description=f"This may take a while. This message will update every {UPDATE_EVERY} messages.")
+                          description=f"This may take a while. This message will update every {UPDATE_EVERY} percent.")
     loading_bar = "🔳🔳🔳🔳🔳🔳🔳🔳🔳🔳"  # 🟨
     embed.set_footer(text=f"{loading_bar}\n0/{all_messages_count} Sent")
     loading_message = await ctx.followup.send(embed=embed)
 
-    webhooks = await ctx.channel.webhooks()
-    history_webhook = None
-    for webhook in webhooks:
-        if webhook.name == "CopyMessages":
-            history_webhook = webhook
-            break
-    else:
-        # create the webhook
-        history_webhook = await ctx.channel.create_webhook(name="CopyMessages", avatar=None, reason="For CopyMessages")
+    history_webhook = await get_webhook(ctx)
 
+    percentiles = []
+    for multiple in range(1, 10):
+        percentiles.append(int(all_messages_count/UPDATE_EVERY)*multiple+1)
     count = 0
     # loop through messages
-    for message in all_messages[start_at-1:]:
+    for message in all_messages[start_at - 1:]:
         try:
             content = None
             files = []
@@ -104,21 +110,24 @@ async def paste(ctx, start_at: Option(int, "The message number to start at. Used
 
             count += 1
 
-            if count % UPDATE_EVERY == 0:
+            if count in percentiles:
                 # edit message if applicable
+                embed = discord.Embed(color=0xFFFF00,
+                                      title=f"⏳ Pasting {all_messages_count} Messages",
+                                      description=f"This may take a while. This message will update every {UPDATE_EVERY} percent.")
                 percent = (count / all_messages_count) * 100
                 percent_string = "{0:02.0f}".format(percent)
                 number_of_squares = int(str(percent_string)[0])
                 number_of_blanks = 10 - number_of_squares
-                squares = "🟨"*number_of_squares + "🔳"*number_of_blanks
+                squares = "🟨" * number_of_squares + "🔳" * number_of_blanks
                 embed.set_footer(text=f"{squares}\n{count}/{all_messages_count} Sent")
                 await loading_message.edit(embed=embed)
+            print(count)
         except Exception as ex:
-            # send error
-            embed.color = 0x880808
-            embed.title = f":x: Failed On Message {count}"
-            embed.description = f"> ```{str(ex)}```"
-            embed.url = message.jump_url
+            embed = discord.Embed(color=0x880808,
+                                  title = f":x: Failed On Message {count}",
+                                  description = f"> ```{str(ex)}```",
+                                  url=message.jump_url)
             await loading_message.edit(embed=embed)
             return
 
